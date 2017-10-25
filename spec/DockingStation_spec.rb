eb86bb1af0547c36d15bace080771b65a113e9f1
Peter Johnstone <peter.wdj@gmail.com>
2017-10-25 18:01:53 +0100

    Removed logic from tests and refactored to two separate tests

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,48 +1,49 @@
 require 'dockingStation.rb'
 
-describe "Instantiating before(:each) variables" do
-before(:each) do
-  bike = Bike.new
-  station = DockingStation.new
+#describe "Instantiating before(:each) variables" do
+  #before(:each) do
+    #bike = Bike.new
+    #station = DockingStation.new
+  #end
 
-end
-
-describe DockingStation do
-  it { should respond_to(:release_bike) }
-  it { should respond_to(:dock)}
-  it { should respond_to(:any_bikes?)}
-end
-
-
-describe DockingStation.new.release_bike do
-  it { is_expected.to be_an_instance_of(Bike)}
-end
-
-describe  DockingStation.new.release_bike.working? do
-  it { is_expected.to eq(true)}
-end
-
-describe "Testing to dock a bike" do
-  station = DockingStation.new
-  bike = station.release_bike
-  station.dock(bike)
-  it "Adds a bike to a docking station" do
-    expect(station.available.length).to eq(1)
+  describe DockingStation do
+    it { should respond_to(:release_bike) }
+    it { should respond_to(:dock)}
+    it { should respond_to(:any_bikes?)}
   end
-end
 
-describe "Allowing members of the public to check if there's any bikes" do
-  station = DockingStation.new
-    if station.available.length > 0
-      it "Should return true as bikes are available" do
-        expect(station.any_bikes?).to eq(true)
-      end
-    else
-      it "Should return false as there are no bikes" do
-        expect(station.any_bikes?).to eq(false)
-      end
+  describe DockingStation.new.release_bike do
+    it { is_expected.to be_an_instance_of(Bike)}
+  end
+
+  describe  DockingStation.new.release_bike.working? do
+    it { is_expected.to eq(true)}
+  end
+
+  describe "Testing to dock a bike" do
+    station = DockingStation.new
+    bike = station.release_bike
+    station.dock(bike)
+    it "Adds a bike to a docking station" do
+      expect(station.available.length).to eq(1)
     end
-    #Write two tests instead of putting logic into one test
-    #Look up before_each and after_each methods for test setup
   end
-end
+
+  describe "Checking if there are any bikes when there are no bikes" do
+    station = DockingStation.new
+    it "Should return false as there are no bikes in the station" do
+      expect(station.any_bikes?).to eq(false)
+    end
+  end
+
+  describe "Checking if there are any bikes when there are bikes" do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    it "Should return true as there are bikes in the station" do
+      expect(station.any_bikes?).to eq(true)
+    end
+  end
+
+  #Look up before_each and after_each methods for test setup - continue to research why these are not working in the other commented code
+#end
