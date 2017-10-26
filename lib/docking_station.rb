3e5c9e382324ad0373aa2f11d4a2025334827e2b
Peter Johnstone <peter.wdj@gmail.com>
2017-10-26 14:55:20 +0100

    Added working code/updated tests for exercise 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,24 @@
 require "./lib/bike.rb"
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+    @capacity = 20
+  end
 
   def release_bike
-    raise 'Sorry, no bikes are available'
+    if @bikes.empty?
+      raise 'Sorry, no bikes are available'
+    end
     Bike.new
   end
 
   def dock(bike)
-    #@bike << bike
-    if self.bike
+    if self.bikes.length >= @capacity
       raise "Sorry, the docking station is full"
     end
-    @bike = bike
+    @bikes << bike
+    @bikes[-1]
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,29 +1,27 @@
 require 'docking_station'
 
-
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock).with(1).argument }
 #  it { is_expected.to respond_to(:bike)}
-  it "docks something" do
+  it "docks a bike" do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
-  it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
+  # it "returns docked bikes" do
+  #   bike = Bike.new
+  #   subject.dock(bike)
+  #   expect(subject.bikes).to eq bike
+  # end
 
   it "raises error message when no bikes are present" do
     expect { subject.release_bike }.to raise_error("Sorry, no bikes are available")
   end
 
   it "raises error message when dock is full" do
-    bike1 = Bike.new
-    subject.dock(bike1)
-    bike2 = Bike.new
-    expect {subject.dock(bike2)}.to raise_error("Sorry, the docking station is full")
+    #20.times { subject.dock(Bike.new) }
+    #bike = Bike.new
+    expect { 20.times { subject.dock(Bike.new) } }.to raise_error("Sorry, the docking station is full")
   end
 
 end
