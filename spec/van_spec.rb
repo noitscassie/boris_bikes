b83d4511351b91461e3caa42fb556ba295e517bb
Allan Wazacz <cazwazacz@gmail.com>
2017-10-27 17:36:36 +0100

    passed test for dropping off fixed bikes from van to station

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,5 +1,16 @@
 require 'van'
+require 'docking_station'
+require 'bike'
 
 describe Van do
   it { is_expected.to respond_to(:collect_bikes).with(1).argument}
+  it 'only drops off 16 bikes when spare_capacity is 16 and the van has 20' do
+    bikes = []
+    20.times {bikes << Bike.new}
+    subject.collect_bikes(bikes)
+    station = DockingStation.new
+    4.times{station.dock(Bike.new)}
+    station.receive_fixed_bikes(subject.drop_bikes(station.spare_capacity))
+    expect(subject.bikes.length).to eq 4
+  end
 end
