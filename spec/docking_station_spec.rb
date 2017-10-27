2c0a655eae743fbde7e64a17542a8a2511b07a3a
Peter Johnstone <peter.wdj@gmail.com>
2017-10-27 11:50:16 +0100

    Passed test ensuring a docking station will not release broken bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock).with(1).argument }
   it "docks a bike" do
-    bike = Bike.new
-    subject.dock(bike)
+    subject.dock(double(:bike))
     expect(subject.bikes[-1]).to eq bike
   end
 
@@ -14,11 +13,11 @@ describe DockingStation do
   end
 
   it "raises error message when dock is full" do
-    expect { 21.times { subject.dock(Bike.new) } }.to raise_error("Sorry, the docking station is full")
+    expect { 21.times { subject.dock(double(:bike)) } }.to raise_error("Sorry, the docking station is full")
   end
 
   it "releases a bike from the docking station" do
-    20.times { subject.dock(Bike.new) }
+    20.times { subject.dock(double(:bike)) }
     bike = subject.bikes[-1]
     expect(subject.release_bike).to eq bike
   end
@@ -42,55 +41,3 @@ describe DockingStation do
   end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# describe DockingStation.new.release_bike do
-#   it { is_expected.to be_an_instance_of(Bike)}
-# end
-#
-# describe  DockingStation.new.release_bike.working? do
-#   it { is_expected.to eq(true)}
-# end
-#
-# describe "Testing to dock a bike" do
-#   station = DockingStation.new
-#   bike = station.release_bike
-#   station.dock(bike)
-#   it "Adds a bike to a docking station" do
-#     expect(station.available.length).to eq(1)
-#   end
-# end
-#
-# describe "Checking if there are any bikes when there are no bikes" do
-#   station = DockingStation.new
-#   it "Should return false as there are no bikes in the station" do
-#     expect(station.any_bikes?).to eq(false)
-#   end
-# end
-#
-# describe "endChecking if there are any bikes when there are bikes" do
-#   station = DockingStation.new
-#   bike = Bike.new
-#   station.dock(bike)
-#   it "Should return true as there are bikes in the station" do
-#     expect(station.any_bikes?).to eq(true)
-#   end
-
-
-  #Look up before_each and after_each methods for test setup - continue to research why these are not working in the other commented code
-#end
